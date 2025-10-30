59003d4f514a20fd6b1de341559987d5981337b1
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2025-10-30 17:24:42 -0400

    Initial Commit

---
+++ WicWacWoe/WicWacWoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WicWacWoeApp.swift
+//  WicWacWoe
+//
+//  Created by Macbook on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WicWacWoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
