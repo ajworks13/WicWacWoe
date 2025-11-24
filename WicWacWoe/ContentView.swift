96092092e83a99ab95d23ac0cf7806b0a19eb7b0
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2025-11-24 18:24:21 -0500

    playing with buttons before making the game

--- WicWacWoe/ContentView.swift
+++ WicWacWoe/ContentView.swift
@@ -7,15 +7,85 @@
 
 import SwiftUI
 
+struct DynamicButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(configuration.isPressed ? Color.red : Color.blue)
+            .foregroundColor(.white)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+           // .frame(width:120, height:80)
+    }
+}
+
+
+struct NavigationButtonStyle: ButtonStyle {
+    init(color: Color = .blue) {
+        self.color = color
+    }
+    let color: Color
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(.white)
+            .padding(.init(top: 10, leading: 90, bottom: 10, trailing: 90))
+            .background(
+                RoundedRectangle(cornerRadius: 50)
+                    .fill(color)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+    }
+}
+
 struct ContentView: View {
+    
+    @State private var box1 = "      "
+    @State private var box2 = "      "
+    @State private var box3 = "      "
+    @State private var box4 = "      "
+    @State private var box5 = "      "
+    @State private var box6 = "      "
+    @State private var count: Int = 0
+    
+    @State private var buttonText = "Press This"
+
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        
+        VStack{
+            
+            Button(action: {
+                // Action to perform
+            }) {
+                HStack {
+                    Image(systemName: "rectangle.grid.1x2.fill")
+                    Text("Button with Image")
+                }
+        
+            }
+            .buttonStyle(NavigationButtonStyle(color: .purple))
+
+            
+            Text("Count: \(count)")
+            Button(action: {
+                count += 1
+                buttonText = "X"
+            }) {
+                Text("\(buttonText)")
+            }
+            .buttonStyle(DynamicButtonStyle())
+            .frame(width:220, height:180)
+            .background(Color.pink)
+
+
+            
+            
         }
-        .padding()
+
     }
 }
 
