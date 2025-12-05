2b9edb21c2ffbca528f5f034769b0c8a23882a1b
Aneudy Consuegra <ilovethe80s90s@yahoo.com>
2025-12-04 19:54:49 -0500

    Trying grid view instead and see how it works compared to how i already had it started.

--- WicWacWoe/ContentView.swift
+++ WicWacWoe/ContentView.swift
@@ -15,7 +15,6 @@ struct DynamicButtonStyle: ButtonStyle {
             .foregroundColor(.white)
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
-           // .frame(width:120, height:80)
     }
 }
 
@@ -28,19 +27,32 @@ struct NavigationButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .foregroundColor(.white)
-            .padding(.init(top: 10, leading: 90, bottom: 10, trailing: 90))
+            .padding(.init(top: 20, leading: 30, bottom: 20, trailing: 30))
             .background(
-                RoundedRectangle(cornerRadius: 50)
+                RoundedRectangle(cornerRadius: 10)
                     .fill(color)
             )
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
             .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+                    
     }
 }
 
+
+
+struct ColorSquare: View {
+    let color: Color
+    
+    var body: some View {
+        color
+        .frame(width: 50, height: 50)
+    }
+}
+
+
 struct ContentView: View {
     
-    @State private var box1 = "      "
+    @State var box1 = " "
     @State private var box2 = "      "
     @State private var box3 = "      "
     @State private var box4 = "      "
@@ -54,37 +66,148 @@ struct ContentView: View {
     
     var body: some View {
         
+        Grid{
+            GridRow{
+                
+                ForEach(0..<3){ _ in
+                    ColorSquare(color: .pink)
+                    
+                }
+                .frame(width:80, height:90)
+
+            }
+            
+            //--------------------------------------------
+            GridRow{
+                
+                ForEach(0..<3){ _ in
+                    ColorSquare(color: .pink)
+                    
+                }
+            }
+            //-------------------------------------------
+            GridRow{
+                
+                ForEach(0..<3){ _ in
+                    ColorSquare(color: .pink)
+                    
+                }
+            }
+        }
         
+            
         
-        VStack{
+        //--------------------------------------------------------------
+        
+        HStack{
+            
             
             Button(action: {
                 // Action to perform
+                box1 = "X"
+
             }) {
                 HStack {
-                    Image(systemName: "rectangle.grid.1x2.fill")
-                    Text("Button with Image")
+                    Text("\(box1)")
+                        .font(.custom("biggestText", size: 110))
+                        .border(Color.red, width: 4)
+                        .frame(width:80, height:90)
+
+
                 }
         
             }
             .buttonStyle(NavigationButtonStyle(color: .purple))
+           // .frame(maxWidth: 90)
+            .border(Color.orange, width: 4)
+            .fixedSize()
+            
+            //------------------------------------------------
+            
+            Button(action: {
+                // Action to perform
+                box2 = "X"
+
+            }) {
+                HStack {
+                    Text("\(box2)")
+                        .font(.custom("biggestText", size: 110))
+                        .border(Color.red, width: 4)
+                        .frame(width:80, height:90)
+
+
+                }
+        
+            }
+            .buttonStyle(NavigationButtonStyle(color: .purple))
+           // .frame(maxWidth: 90)
+            .border(Color.orange, width: 4)
+            .fixedSize()
+            
+            //-------------------------------------------------
+            
+            Button(action: {
+                // Action to perform
+                box1 = "X"
+
+            }) {
+                HStack {
+                    Text("\(box1)")
+                        .font(.custom("biggestText", size: 110))
+                        .border(Color.red, width: 4)
+                        .frame(width:80, height:90)
+
+
+                }
+        
+            }
+            .buttonStyle(NavigationButtonStyle(color: .purple))
+           // .frame(maxWidth: 90)
+            .border(Color.orange, width: 4)
+            .fixedSize()
+            
+            //------------------------------------------------
+            
+            Button(action: {
+                // Action to perform
+                box1 = "X"
+
+            }) {
+                HStack {
+                    Text("\(box1)")
+                        .font(.custom("biggestText", size: 110))
+                        .border(Color.red, width: 4)
+                        .frame(width:80, height:90)
+
+
+                }
+        
+            }
+            .buttonStyle(NavigationButtonStyle(color: .purple))
+           // .frame(maxWidth: 90)
+            .border(Color.orange, width: 4)
+            .fixedSize()
 
             
-            Text("Count: \(count)")
-            Button(action: {
-                count += 1
-                buttonText = "X"
-            }) {
-                Text("\(buttonText)")
-            }
-            .buttonStyle(DynamicButtonStyle())
-            .frame(width:220, height:180)
-            .background(Color.pink)
+//            Text("Count: \(count)")
+//            Button(action: {
+//                count += 1
+//                buttonText = "x"
+//            }) {
+//                Text("\(buttonText)")
+//            }
+//            .buttonStyle(DynamicButtonStyle())
+//            .frame(width:220, height:180)
+//            .background(Color.pink)
 
 
             
             
         }
+        .frame(maxWidth: 360, maxHeight: 500)
+        .border(Color.blue, width: 4)
+        
+
 
     }
 }
